aaa77b815547ca8f7ff85d7a6a2ba442e185a598
Yoshiki-Ando <56804093+Yoshiki-Ando@users.noreply.github.com>
2020-08-25 17:44:11 -0400

    Add files via upload
    I added the function 'ParaDensity(paraValues )' which evaluates the density for each parameter in a vector (paraValues) and yields a vector of densities. I hope it doesn't have coding error but can't guarantee at this moment ^^;

--- Prior.jl
+++ Prior.jl
@@ -1,7 +1,6 @@
 ## Draw Parameters From Prior Distribution
 # August 19th, Yoshiki
-
-using Distributions
+using Distributions, Plots
 ## Define Functions
 # These functions draw a random variable for each distribution
 # given mean (μ) and std (σ)
@@ -107,7 +106,177 @@ parameters =
  return parameters
 end
 
-priorpara= = DrawParaFromPrior()
+priorpara = DrawParaFromPrior()
 
+# Check that this function generates random variables with targetter mean
 # aa = [DrawParaFromPrior() for i=1:10000]
 # println(mean(aa))
+
+## Evaluate Density of each value
+
+γ  = rand(Normal(0.4, 0.05))/100
+pdf(Normal(0.4, 0.05), 100γ )
+
+# Check if it works properly
+vγ   = -3:0.01:3
+vpdf = pdf(Normal(), vγ )
+plot(vγ,vpdf)
+
+## Create Functions to evaluate pdf
+# using Pkg ; Pkg.add("SpecialFunctions")
+# using SpecialFunctions
+function myGammaPdf(μ, σ, value)
+   α         = μ^2 / σ^2 ; # compute α in Γ dist
+   β         = σ^2 / μ ;   # compute β in Γ dist
+#   pdf_temp  = value^(α-1)*exp(-value/β)/(gamma(α)*β^α)
+   pdf_temp  = pdf(Gamma(α, β), value )
+   return pdf_temp
+end
+
+# Check if it works well
+α1 = 2; β1 = 2;
+μ1 = α1 * β1; σ1 = sqrt(α1 * β1^2)
+vGamma   = reshape(0.01:0.01:10, length(0.01:0.01:10), 1)
+vpdfGamma= [myGammaPdf(μ1, σ1, vGamma[i,1]) for i=1:length(vGamma)]
+plot(vGamma[:,1],vpdfGamma[:,1])
+
+## Beta Function
+function myBetaPdf(μ,σ, value) #return a draw from Beta Dist with (μ,σ)
+   α         = ( (1-μ)/σ^2 - 1/μ ) * μ^2 ; # compute α in Β dist
+   β        = α * (1/μ - 1);              # compute β in Β dist
+   pdf_temp = pdf(Beta(α, β),value)
+   return pdf_temp
+end
+
+# Check if it works well
+α2 = 2; β2 = 2;
+μ2 = α2/(α2+β2)
+σ2 = sqrt(α2 * β2 / ( (α2+β2)^2*(α2+β2+1))  )
+
+vBeta    = [0.01:0.01:1]
+vpdfBeta = [myBetaPdf(μ2, σ2, vBeta[i]) for i=1:length(vBeta)]
+plot(vBeta[:,1],vpdfBeta[:,1])
+
+
+## Uniform Distribution
+function myUniPdf(μ, σ, value)
+   β          =  μ + 3^0.5 * σ
+   α          =  2 * μ - β
+   pdf_temp   = pdf(Uniform(α, β),value)
+   return pdf_temp
+end
+
+# Check if it works well
+α3 = -1.749; β3 = 1.749;
+μ3 = 0.5*(α3 + β3)
+σ3 = sqrt( (α3-β3)^2/12 )
+
+vUni     = [-2:0.01:2]
+vpdfUni  = [myUniPdf(μ3, σ3, vUni[i]) for i=1:length(vUni)]
+plot(vUni[:,1],vpdfUni[:,1])
+
+
+## Inverse Gamma Distribution
+function myInvΓPdf(μ,σ, value)
+   α = μ^2/σ^2 + 2
+   θ = μ* ( α -1)
+   pdf_temp = pdf(InverseGamma(α,θ),value)
+   return pdf_temp
+end
+
+# Check if it works well
+α4 = 3; β4 = 1;
+μ4 = β4/(α4-1)
+σ4 = sqrt( β4^2/((α4-1)^2*(α4-2)) )
+
+vInvG     = [0.01:0.01:3]
+vpdfInvG  = [myInvΓPdf(μ4, σ4, vInvG[i]) for i=1:length(vInvG)]
+plot(vInvG[:,1],vpdfInvG[:,1])
+
+
+
+## Function to Evaluate the density for each parameter value
+function ParaDensity(paraValues )
+# Preferences and HHs
+γ_pdf  = pdf(Normal(0.4, 0.05),100*paraValues[1]) # Steady State ln growth
+ξ_pdf  = myGammaPdf(2.0,0.5,paraValues[2])  # Inverse Frisch labor elasticity
+θ_pdf  = myBetaPdf(0.5,0.2,paraValues[3]) # Habit formation
+μ_pdf  = myBetaPdf(0.3,0.1,paraValues[4]) # Fraction of non-savers
+α_G_pdf= myUniPdf(0.0,1.01,paraValues[5]) # Substitutability of private/public consumption
+
+# Frictions and production
+ψ_pdf   = myBetaPdf(0.6,0.15,paraValues[6]) # Capital utilization
+s_pdf   = pdf(Normal(6,1.5),paraValues[7]) # Investment adjustment cost
+ω_p_pdf = myBetaPdf(0.5,0.2,paraValues[8]) # Price stickiness
+ω_w_pdf = myBetaPdf(0.5,0.2,paraValues[9]) # Wage stickiness
+χ_p_pdf = myBetaPdf(0.5,0.2,paraValues[10]) # Price partial indexation
+χ_w_pdf = myBetaPdf(0.5,0.2,paraValues[11]) # Wage partial indexation
+
+# Monetary policy
+ϕ_πM_pdf = pdf(Normal(1.5,0.2),paraValues[12]) # Interest rate response to inflation, regime M
+ϕ_πF_pdf = myBetaPdf(0.5,0.15,paraValues[13]) # Interest rate response to inflation, regime F
+ϕ_y_pdf  = pdf(Normal(0.125,0.05),paraValues[14]) # Interest rate response to output
+ρ_r_pdf  = myBetaPdf(0.5,0.2,paraValues[15]) # Response to lagged interest rate
+
+# Fiscal policy
+γ_GM_pdf = pdf(Normal(0.15,0.1),paraValues[16]) # Debt response for expenditures, regime M
+γ_KM_pdf = pdf(Normal(0.15,0.1),paraValues[17]) # Debt response for capital taxes, regime M
+γ_LM_pdf = pdf(Normal(0.15,0.1),paraValues[18]) # Debt response for labor taxes, regime M
+γ_ZM_pdf = pdf(Normal(0.15,0.1),paraValues[19]) # Debt response for transfers, regime M
+γ_GF_pdf = pdf(Normal(0.0,0.001),paraValues[20]) # Debt response for expenditures, regime F
+γ_KF_pdf = pdf(Normal(0.0,0.001),paraValues[21]) # Debt response for capital taxes, regime F
+γ_LF_pdf = pdf(Normal(0.0,0.001),paraValues[22]) # Debt response for labor taxes, regime F
+γ_ZF_pdf = pdf(Normal(0.0,0.001),paraValues[23]) # Debt response for transfers, regime F
+ρ_G_pdf  = myBetaPdf(0.5,0.2,paraValues[24]) # Lagged response for expenditures
+ρ_K_pdf  = myBetaPdf(0.5,0.2,paraValues[25]) # Lagged response for capital taxes
+ρ_L_pdf  = myBetaPdf(0.5,0.2,paraValues[26]) # Lagged response for labor taxes
+ρ_Z_pdf  = myBetaPdf(0.5,0.2,paraValues[27]) # Lagged response for transfers
+
+# Shocks
+ρ_a_pdf   = myBetaPdf(0.5,0.2,paraValues[28]) # Autoregressive parameter, technology shock
+ρ_b_pdf   = myBetaPdf(0.5,0.2,paraValues[29]) # Autoregressive parameter, preference shock
+ρ_i_pdf   = myBetaPdf(0.5,0.2,paraValues[30]) # Autoregressive parameter, investment shock
+ρ_p_pdf   = myBetaPdf(0.5,0.2,paraValues[31]) # Autoregressive parameter, price markup shock
+ρ_w_pdf   = myBetaPdf(0.5,0.2,paraValues[32]) # Autoregressive parameter, wage markup shock
+ρ_em_pdf  = myBetaPdf(0.5,0.15,paraValues[33]) # Autoregressive parameter, monetary policy shock
+ρ_eg_pdf  = myBetaPdf(0.5,0.15,paraValues[34]) # Autoregressive parameter, government consumption shock
+ρ_ez_pdf  = myBetaPdf(0.5,0.15,paraValues[35]) # Autoregressive parameter, transfers shock
+σ_a_pdf   = myInvΓPdf(0.1,1,100*paraValues[36]) # Standard deviation, technology shock
+σ_b_pdf   = myInvΓPdf(0.1,1,100*paraValues[37]) # Standard deviation, preference shock
+σ_i_pdf   = myInvΓPdf(0.1,1,100*paraValues[38]) # Standard deviation, investment shock
+σ_p_pdf   = myInvΓPdf(0.1,1,100*paraValues[39]) # Standard deviation, price markup shock
+σ_w_pdf   = myInvΓPdf(0.1,1,100*paraValues[40]) # Standard deviation, wage markup shock
+σ_em_pdf  = myInvΓPdf(0.1,1,100*paraValues[41]) # Standard deviation, monetary policy shock
+σ_eg_pdf  = myInvΓPdf(0.1,1,100*paraValues[42]) # Standard deviation, government consumption shock
+σ_ez_pdf  = myInvΓPdf(0.1,1,100*paraValues[43]) # Standard deviation, transfers shock
+
+## Create an array (vector) of parameters
+paraPdf =
+   [γ_pdf,ξ_pdf,θ_pdf,μ_pdf,α_G_pdf,ψ_pdf,s_pdf,ω_p_pdf,ω_w_pdf,χ_p_pdf,χ_w_pdf,ϕ_πM_pdf,ϕ_πF_pdf,ϕ_y_pdf,ρ_r_pdf]
+paraPdf =
+  push!(paraPdf,γ_GM_pdf,γ_KM_pdf,γ_LM_pdf,γ_ZM_pdf,γ_GF_pdf,γ_KF_pdf,γ_LF_pdf,γ_ZF_pdf)
+paraPdf =
+  push!(paraPdf,ρ_G_pdf,ρ_K_pdf,ρ_L_pdf,ρ_Z_pdf,ρ_a_pdf,ρ_b_pdf,ρ_i_pdf,ρ_p_pdf,ρ_w_pdf,ρ_em_pdf,ρ_eg_pdf,ρ_ez_pdf)
+paraPdf =
+  push!(paraPdf,σ_a_pdf,σ_b_pdf,σ_i_pdf,σ_p_pdf,σ_w_pdf,σ_em_pdf,σ_eg_pdf,σ_ez_pdf)
+
+# num_parameters = length(paraPdf)
+ return paraPdf
+end
+
+pdf1 = ParaDensity(priorpara )
+
+## Test if it works properly
+Ntry        = 1000
+TryMatrix   = zeros(length(priorpara), Ntry)
+
+for jj = 1:Ntry
+priorpara1  = DrawParaFromPrior()
+paraPdf1    = ParaDensity(priorpara1 )
+
+vPrior       = [DrawParaFromPrior() for i = 1:1000]
+vParaPdf     = [ParaDensity(vPrior[i] ) for i = 1:1000]
+vPdfRelative = [vParaPdf[i]./paraPdf1 for i = 1:1000]
+TryMatrix[:,jj] = mean(vPdfRelative)
+end
+println(mean(TryMatrix,dims = 2))
