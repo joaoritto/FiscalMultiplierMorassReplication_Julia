fea09378748f080acd47ec93a57a2234fcd7e752
joaoritto <59777732+joaoritto@users.noreply.github.com>
2020-09-17 08:05:24 -0500

    Add files via upload
    Added using SparseArrays

--- main.jl
+++ main.jl
@@ -1,16 +1,22 @@
 # Main file
 
-path="/home/joaoritto/Dropbox/Economics/Books_and_notes/SecondYear/Macroeconometrics/TermPaper/"
+path="C:\\Users\\joaor\\Dropbox\\Economics\\Books_and_notes\\SecondYear\\Macroeconometrics\\TermPaper\\722TermPaper-master\\"
 
-using Statistics, LinearAlgebra, Distributions
+using Statistics, LinearAlgebra, Distributions, SparseArrays, JLD
 
 include(path*"prior.jl")
 include(path*"model.jl")
 include(path*"PVmultiplier.jl")
 include(path*"modelrestrictions.jl")
 include(path*"priorpredictiveanalysis.jl")
-include(path*"Kalman.jl")
+#include(path*"Kalman.jl")
 
-N=1000
+N=20000
 
 Table_y,Table_c,Table_i=table3(N,path)
+
+cd(path) # go to the current directory
+
+save("file/PriorPredictiveN20000_model3.jld","OutMul",
+Table_y, "ConsMul", Table_c,
+"InvMul",Table_i) # save in the folder (I created a folder "file")
